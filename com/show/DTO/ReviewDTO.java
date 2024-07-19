a7e410861eb21e43eca3490141db77825d34f81e
10-2\MBC <cxas6673@naver.com>
2024-07-19 18:28:57 +0900

    TeamProject(07.19 v1.0.0)

--- com/show/DTO/ReviewDTO.java
+++ com/show/DTO/ReviewDTO.java
@@ -5,15 +5,6 @@ public class ReviewDTO {
 	private String nickName;
 	private String starPoint; // 별점
 	private String review;
-	private String title;
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
 
 	// 메서드
 	public String getNickName() {
